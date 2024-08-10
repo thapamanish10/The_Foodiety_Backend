e9addc7502d1809e5b5b740cc0fb0d3311d16e16
Manish Thapa <gitlabthapa@gmail.com>
2024-08-10 19:24:25 +0545

    Fourth Commit

--- routes/auth.php
+++ routes/auth.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\auth\AuthenticatedSessionController;
+use App\Http\Controllers\auth\ConfirmablePasswordController;
+use App\Http\Controllers\auth\EmailVerificationNotificationController;
+use App\Http\Controllers\auth\EmailVerificationPromptController;
+use App\Http\Controllers\auth\NewPasswordController;
+use App\Http\Controllers\auth\PasswordController;
+use App\Http\Controllers\auth\PasswordResetLinkController;
+use App\Http\Controllers\auth\RegisteredUserController;
+use App\Http\Controllers\auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
